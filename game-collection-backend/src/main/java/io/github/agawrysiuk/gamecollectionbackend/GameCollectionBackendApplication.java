0c03f964a90f211338af6054db32c82e6127e92a
dlrzz <arkad.gawry@gmail.com>
2020-02-15 12:19:46 +0100

    Initial commit

---
+++ game-collection-backend/src/main/java/io/github/agawrysiuk/gamecollectionbackend/GameCollectionBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.agawrysiuk.gamecollectionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameCollectionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameCollectionBackendApplication.class, args);
+	}
+
+}
---
+++ game-collection-backend/src/test/java/io/github/agawrysiuk/gamecollectionbackend/GameCollectionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.agawrysiuk.gamecollectionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameCollectionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
