3ade6e82657d388e0d0885bc43c3ca0121c145b8
dlrzz <arkad.gawry@gmail.com>
2020-03-02 21:41:50 +0100

    Added simple service to get all games

---
+++ game-collection-frontend/src/app/service/game-downloader.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Game} from "../dto/game";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameDownloaderService {
+
+  private readonly allGamesUrl;
+
+  constructor(private http: HttpClient) {
+    this.allGamesUrl = 'https://api.rawg.io/api/games';
+  }
+
+  public getAllGames(): Observable<Game[]> {
+    return this.http.get<Game[]>(this.allGamesUrl);
+  }
+}
