c2ee95fe9802922979d9b30c49cba8f566959be1
dlrzz <arkad.gawry@gmail.com>
2020-03-14 10:58:58 +0100

    Fixed unused imports

--- game-collection-backend/src/main/java/io/github/agawrysiuk/gamecollectionbackend/dto/Genres.java
+++ game-collection-backend/src/main/java/io/github/agawrysiuk/gamecollectionbackend/dto/Genres.java
@@ -1,6 +1,5 @@
 package io.github.agawrysiuk.gamecollectionbackend.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
--- game-collection-backend/src/test/java/io/github/agawrysiuk/gamecollectionbackend/service/GameServiceWriteTest.java
+++ game-collection-backend/src/test/java/io/github/agawrysiuk/gamecollectionbackend/service/GameServiceWriteTest.java
@@ -7,11 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 import java.util.LinkedHashSet;
 import java.util.Optional;
-import java.util.Set;
 
 @SpringBootTest
 @ActiveProfiles("test")
