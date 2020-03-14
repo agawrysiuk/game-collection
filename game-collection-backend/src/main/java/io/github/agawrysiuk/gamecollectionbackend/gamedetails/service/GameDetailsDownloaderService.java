b63f3f9f58f1964fc3197b324d70299038b46e44
dlrzz <arkad.gawry@gmail.com>
2020-03-14 14:21:17 +0100

    Added screenshot download

--- game-collection-backend/src/main/java/io/github/agawrysiuk/gamecollectionbackend/gamedetails/service/GameDetailsDownloaderService.java
+++ game-collection-backend/src/main/java/io/github/agawrysiuk/gamecollectionbackend/gamedetails/service/GameDetailsDownloaderService.java
@@ -1,25 +1,41 @@
 package io.github.agawrysiuk.gamecollectionbackend.gamedetails.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.agawrysiuk.gamecollectionbackend.gamedetails.model.GameDetails;
+import io.github.agawrysiuk.gamecollectionbackend.gamedetails.model.ScreenShot;
 import io.github.agawrysiuk.gamecollectionbackend.gamedetails.utils.GameNameFormatter;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Set;
 
 @Service
 public class GameDetailsDownloaderService {
     private final static String GAME_URL = "https://api.rawg.io/api/games/";
     private final static String SCREENSHOTS_URL = "/screenshots";
+    private ObjectMapper objectMapper;
 
-    public GameDetails readJsonFromUrl(String gameName) throws IOException {
-        URL rawgUrl = new URL(GAME_URL + GameNameFormatter.changeTitleToURLFormat(gameName));
-        ObjectMapper objectMapper = new ObjectMapper();
+    @PostConstruct
+    public void postInit() {
+        objectMapper = new ObjectMapper();
         objectMapper.configure(
                 DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    public GameDetails getGameDetails(String gameName) throws IOException {
+        URL rawgUrl = new URL(GAME_URL + GameNameFormatter.changeTitleToURLFormat(gameName));
         return objectMapper.readValue(rawgUrl, GameDetails.class);
+    }
+
+    public Set<ScreenShot> getScreenShots(String gameName) throws IOException {
+        URL rawgUrl = new URL(GAME_URL + GameNameFormatter.changeTitleToURLFormat(gameName) + SCREENSHOTS_URL);
+        JsonNode json = objectMapper.readTree(rawgUrl);
+        return objectMapper.readValue(json.get("results").traverse(),new TypeReference<Set<ScreenShot>>(){});
     }
 
 }
--- game-collection-backend/src/test/java/io/github/agawrysiuk/gamecollectionbackend/game/service/GameDetailsDownloaderServiceTest.java
+++ game-collection-backend/src/test/java/io/github/agawrysiuk/gamecollectionbackend/game/service/GameDetailsDownloaderServiceTest.java
@@ -1,22 +1,40 @@
 package io.github.agawrysiuk.gamecollectionbackend.game.service;
 
-import io.github.agawrysiuk.gamecollectionbackend.gamedetails.service.GameDetailsDownloaderService;
 import io.github.agawrysiuk.gamecollectionbackend.gamedetails.model.GameDetails;
+import io.github.agawrysiuk.gamecollectionbackend.gamedetails.model.ScreenShot;
+import io.github.agawrysiuk.gamecollectionbackend.gamedetails.service.GameDetailsDownloaderService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.util.Set;
 
+@SpringBootTest
 class GameDetailsDownloaderServiceTest {
+    String gameName = "Max Payne";
+
+    @Autowired
+    GameDetailsDownloaderService detailsService;
 
     @Test
-    void readJsonFromUrl() {
-        String gameName = "Max Payne";
-        GameDetailsDownloaderService detailsService = new GameDetailsDownloaderService();
+    void given_GameName_Expect_GameDetailsObject() {
         try {
-            GameDetails gameDetails = detailsService.readJsonFromUrl(gameName);
+            GameDetails gameDetails = detailsService.getGameDetails(gameName);
             System.out.println(gameDetails);
             Assertions.assertNotNull(gameDetails);
+        } catch (IOException e) {
+            e.printStackTrace();
+        };
+    }
+
+    @Test
+    void given_GameName_Expect_ScreenshotsForTheGame() {
+        try {
+            Set<ScreenShot> screenShots = detailsService.getScreenShots(gameName);
+            Assertions.assertNotNull(screenShots);
+            System.out.println(screenShots.toString());
         } catch (IOException e) {
             e.printStackTrace();
         };
